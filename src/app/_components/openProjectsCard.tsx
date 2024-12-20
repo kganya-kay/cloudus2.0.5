8fec8696ee7b75c672f1dd2b8cd75fd96d624763
kganya <kganyakekana@gmail.com>
2024-12-19 16:21:46 -0800

    dasyh3

--- src/app/_components/openProjectsCard.tsx
+++ src/app/_components/openProjectsCard.tsx
@@ -38,7 +38,7 @@ const card = (
 
 export default function OutlinedCard() {
   return (
-    <Box sx={{ minWidth: 275 }} >
+    <Box sx={{ minWidth: 50 }} >
       <Card variant="outlined" >{card}</Card>
     </Box>
   );
