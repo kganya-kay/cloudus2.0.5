58f006cdd98dbe47a24a9072835525286998480f
kganya <kganyakekana@gmail.com>
2024-12-24 00:57:07 -0800

    all projects card

--- src/app/_components/allProjects.tsx
+++ src/app/_components/allProjects.tsx
@@ -7,59 +7,81 @@ export default function AllProjects() {
 
   return (
     <div>
-      {allProjects.length == 0
-        ? "You Have No Projects Yet"
-        : ""}
+      {allProjects.length == 0 ? "You Have No Projects Yet" : ""}
       <ul
         role="list"
         className="flex flex-col gap-2 divide-y divide-white px-4"
       >
         {allProjects.map((project) => (
-          <li key={project.id} className="gap-x-6 py-5 bg-gray-100 p-3 rounded-md">
-            <div className="flex justify-between ">
-              <div className="flex min-w-0 gap-x-4">
+          <li
+            key={project.id}
+            className="gap-x-6 rounded-md bg-gray-100 p-3 py-5 "
+          > 
+            <div className="bg-slate-400 mb-3 rounded-sm">
+              <p className=" text-center text-sm text-white font-bold">{project.name}</p>
+            </div>
+            <div className="flex justify-between">
+              <div className="min-w-0">
                 <img
                   alt=""
                   src={project.image}
                   className="size-12 flex-none rounded-full"
                 />
-                <div className="min-w-0 flex-auto">
-                  <p className="text-sm/6 font-semibold text-gray-900">
-                    {project.name}
-                  </p>
-                  <p className="text-sm/6 font-semibold rounded-se-xl bg-red-500 px-1">
-                    R {project.price}
-                  </p>
-                </div>
               </div>
-              <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                <time dateTime={project.updatedAt.toDateString()}>
-                  {project.updatedAt.toLocaleString()}
-                </time>
+              <div className="min-w-0">
+                <p className="rounded-se-xl bg-red-500 px-1 text-sm/6 font-semibold">
+                  Project Budget: R {project.price}
+                </p>
+
+                <p className="text-sm/6 font-semibold text-gray-900">
+                  {project.type}
+                </p>
               </div>
             </div>
             <div className="flex justify-between">
               <div>
-               <img src={project.image} className="rounded-xl" alt="" />
+                <img
+                  src={project.links[0]}
+                  className="size-40 rounded-xl"
+                  alt=""
+                />
               </div>
               <div>
-               <img src={project.image} className="rounded-xl" alt="" />
+                <img
+                  src={project.links[1]}
+                  className="size-40 rounded-xl"
+                  alt=""
+                />
               </div>
               <div>
-               <img src={project.image} className="rounded-xl" alt="" />
+                <img
+                  src={project.links[2]}
+                  className="size-40 rounded-xl"
+                  alt=""
+                />
               </div>
-              
             </div>
-            <div className=" w-full">
-              <p className="mt-1 truncate text-xs/5 text-black text-center max-w-56 ">
+            <div className="flex justify-center">
+              <p className="mt-1 max-w-56 truncate text-center text-xs/5 text-black">
                 {project.description}
               </p>
             </div>
             <div className="justify-self-center py-3">
-              <Button style={{ minWidth: '200px', minHeight: '30px', position:"inherit"}} variant="contained" className=" w-full bg-slate-400" href={`projects/${project.id}`}>
+              <Button
+                style={{
+                  minWidth: "200px",
+                  minHeight: "30px",
+                  position: "inherit",
+                }}
+                variant="contained"
+                className="w-full bg-slate-400"
+                href={`projects/${project.id}`}
+              >
                 View Project
               </Button>
             </div>
+
+                    
           </li>
         ))}
       </ul>
