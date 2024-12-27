f576a1ce21396befdc39708d6811b4e1e52dedba
kganya <kganyakekana@gmail.com>
2024-12-27 04:08:13 -0800

    add offline order2

--- src/app/shop/orders/[orderId]/page.tsx
+++ src/app/shop/orders/[orderId]/page.tsx
@@ -6,27 +6,24 @@ import { useState } from "react";
 import { api } from "~/trpc/react";
 
 export default function LatestProject() {
-
   const params = useParams();
   let itemId = 0;
   let price = 0;
 
   if (params.orderId) {
-      itemId = parseInt(params.orderId.toString())
-      
+    itemId = parseInt(params.orderId.toString());
   } else {
-     itemId = 5
+    itemId = 5;
   }
-  
-  
-  const selectedItem = api.shopItem.select.useQuery({id : itemId });
+
+  const selectedItem = api.shopItem.select.useQuery({ id: itemId });
   console.log(selectedItem);
   const utils = api.useUtils();
 
   if (selectedItem.data?.price) {
-    price = selectedItem.data?.price
-  } 
-  
+    price = selectedItem.data?.price;
+  }
+
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [type, setType] = useState("");
@@ -38,36 +35,42 @@ export default function LatestProject() {
       setDescription("");
       setType("");
       setContactNumber(0);
-      alert("Order Created Successfully")
+      alert("Order Created Successfully");
     },
   });
 
   return (
-    <div className="w-full max-w-xl gap-4 justify-self-center justify-center p-2">
+    <div className="w-full max-w-xl justify-center gap-4 justify-self-center p-2">
       {selectedItem ? (
         <>
-        <div className="bg-gray-500 rounded-t-lg">
-        <h1 className=" text-center truncate text-blue-500 font-extrabold">
-            Confirm Order:     
-          </h1>
-          <p className="text-white text-center font-semibold">{selectedItem.data?.name}</p>
-        </div>
-          
-       
+          <div className="rounded-t-lg bg-gray-500">
+            <h1 className="truncate text-center font-extrabold text-blue-500">
+              Confirm Order:
+            </h1>
+            <p className="text-center font-semibold text-white">
+              {selectedItem.data?.name}
+            </p>
+          </div>
+
           <div className="flex-col justify-between justify-items-center border-y border-y-white py-1">
-            <div className="justify-items-center">
-            <img
-            alt=""
-            src={selectedItem.data?.image}
-            className="size-12 flex-none rounded-full bg-slate-400 justify-self-center"
-          />
+            <div className="justify-center">
+              <img
+                alt=""
+                src={selectedItem.data?.image}
+                className="size-12  justify-self-center rounded-full bg-slate-400"
+              />
             </div>
-            <div><p className="text-sm text-center">{selectedItem.data?.description}</p></div>
+            <div>
+              <p className="text-center text-sm">
+                {selectedItem.data?.description}
+              </p>
+            </div>
           </div>
           <br />
-          <p className="font-semibold bg-green-500 p-2">Total: R {selectedItem.data?.price}</p>
-          
-        </> 
+          <p className="bg-green-500 p-2 font-semibold">
+            Total: R {selectedItem.data?.price}
+          </p>
+        </>
       ) : (
         <p>You have no Orders yet.</p>
       )}
@@ -75,7 +78,14 @@ export default function LatestProject() {
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          createOrder.mutate({ name, description, type, contactNumber, itemId, price });
+          createOrder.mutate({
+            name,
+            description,
+            type,
+            contactNumber,
+            itemId,
+            price,
+          });
         }}
         className="flex flex-col gap-2"
       >
