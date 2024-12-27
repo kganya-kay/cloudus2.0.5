ece1ddccbcb9c2650406e917dfa6c20395dab897
kganya <kganyakekana@gmail.com>
2024-12-27 05:17:26 -0800

    add offline order

--- src/app/_components/allShopItems.tsx
+++ src/app/_components/allShopItems.tsx
@@ -12,46 +12,68 @@ export default function AllShopItems() {
     >
       {allShopItems.map((shopItem) => (
         <div key={shopItem.id} className="flex flex-col justify-between">
-          <div className="bg-gray-100 p-3 rounded-md">
-            <li  className="flex justify-between gap-x-6 py-5 ">
-              <div className="flex min-w-0 gap-x-4">
-                <img
-                  alt=""
-                  src={shopItem.image}
-                  className="size-12 flex-none rounded-full bg-slate-400"
-                />
-                <div className="min-w-0 flex-auto">
+          <div className="rounded-md bg-gray-100 p-3">
+            <li className="flex  py-5">
+              <div className="flex w-full  justify-between ">
+                <div>
+                  <img
+                    alt=""
+                    src={shopItem.image}
+                    className="size-12  rounded-full bg-slate-400"
+                  />
+                </div>
+
+                <div className="  justify-between">
+                  <p className="rounded-se-xl bg-red-500 px-1 text-sm/6 font-semibold">
+                    R {shopItem.price}
+                  </p>
                   <p className="text-sm/6 font-semibold text-gray-900">
                     {shopItem.name}
                   </p>
-                  <p className="text-sm/6 font-semibold rounded-se-xl bg-red-500 px-1">
-                    R {shopItem.price}
-                  </p>
-                  
                 </div>
-              </div>
-              <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                <p className="text-sm/6 text-gray-900">{shopItem.type}</p>
-
-                <p className="mt-1 text-xs/5 text-blue-500  ">
-                  Cloudus Product
-                </p>
               </div>
             </li>
             <div className="flex justify-between">
-              <div><img className="size-40 md:size-60 rounded-lg p-1" src={shopItem.links[0]} alt="" /></div>
-              <div><img className="size-40 md:size-60 rounded-lg p-1" src={shopItem.links[1]} alt="" /></div>
-              <div><img className="size-40 md:size-60 rounded-lg p-1" src={shopItem.links[2]} alt="" /></div>
+              <div>
+                <img
+                  className="size-40 rounded-lg p-1 md:size-60"
+                  src={shopItem.links[0]}
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  className="size-40 rounded-lg p-1 md:size-60"
+                  src={shopItem.links[1]}
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  className="size-40 rounded-lg p-1 md:size-60"
+                  src={shopItem.links[2]}
+                  alt=""
+                />
+              </div>
             </div>
-            <br/>
+            <br />
             <div className="flex justify-center">
-            <p className="mt-1 truncate text-xs/5 text-gray-500 text-center max-w-56">
-                    {shopItem.description}
-                  </p>
+              <p className="mt-1 max-w-56 truncate text-center text-xs/5 text-gray-500">
+                {shopItem.description}
+              </p>
             </div>
-            
+
             <div className="justify-self-center py-3">
-              <Button style={{ minWidth: '200px', minHeight: '30px', position:"inherit"}} variant="contained" className=" w-full bg-slate-400" href={`shop/orders/${shopItem.id}`}>
+              <Button
+                style={{
+                  minWidth: "200px",
+                  minHeight: "30px",
+                  position: "inherit",
+                }}
+                variant="contained"
+                className="w-full bg-slate-400"
+                href={`shop/orders/${shopItem.id}`}
+              >
                 Order Now!
               </Button>
             </div>
