71658084bca1f6b38d1ff6f0d2c2fb7ef23802db
Doctor Kekana <doctor.k@bsky.co.za>
2026-01-07 23:18:44 +0200

    proj

--- src/app/_components/specials.tsx
+++ src/app/_components/specials.tsx
@@ -52,7 +52,7 @@ export default function Special() {
             <div className="mt-10 flex flex-wrap items-center justify-center gap-3 lg:justify-start">
               {/* Internal CTA */}
               <Link
-                href="/shop/orders/6"
+                href="/shop/orders/2"
                 className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
               >
                 Get started
